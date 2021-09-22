59596cd3dc88ef4f169a377d7552ba86ac2fdeed
Avinesh Pandey <65820597+avinesh2101@users.noreply.github.com>
2021-09-23 00:23:36 +0530

    threads 2

---
+++ threadsIssues.py
@@ -0,0 +1,26 @@
+import threading
+
+COUNT =0
+
+def calculate_count(arg):
+    print("{}:begin".format(arg))
+    global COUNT
+    for _ in range(1000000):
+        COUNT = COUNT +1
+    print("{}:ends".format(arg))
+
+def main():
+    print("main begin:COUNT = {}".format(COUNT))
+    t1 = threading.Thread(target=calculate_count, args=("t1",))#creating of threads
+    t2 = threading.Thread(target=calculate_count, args=("t2",))
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
+
+    print("main dont: COUNT = {}".format(COUNT))
+
+if __name__ == "__main__":
+    main()
